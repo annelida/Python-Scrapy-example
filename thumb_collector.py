689650e9d5f7508c989d92dd0298413c11916eb0
annelida <viqsiq@gmail.com>
2016-11-03 19:21:39 +0200

    Loop was added. Files names generate.

--- thumb_collector.py
+++ thumb_collector.py
@@ -1,5 +1,16 @@
 import requests
+import time
 
-resp = requests.get('http://www.apercite.fr/api/apercite/120x90/yes/www.iacono.fr')
-img = resp.raw
-# save raw img on hard disc
+f = open('test_words.csv')
+for url in f:
+    url = url.strip()
+    resp = requests.get('http://www.apercite.fr/api/apercite/320x240/yes/url')
+    img = resp.raw
+    # save raw img on hard disc
+    time.sleep(20)
+    filename = url.split("//")[-1].replace('/', '_') + '.jpg'
+    print filename
+    # output = open("images/filename", "wb")
+    # output = open("images/file01.jpg", "wb")
+    # output.write(resp.content)
+    # output.close()
