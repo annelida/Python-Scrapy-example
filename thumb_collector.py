b30081fa8da8143cc85ade70484da41c3108acf0
annelida <viqsiq@gmail.com>
2016-11-04 07:53:10 +0200

    Loggings settings was changed

--- thumb_collector.py
+++ thumb_collector.py
@@ -6,17 +6,17 @@ hdlr = logging.FileHandler('thumbnails.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 
 f = open('test_words.csv')
 for url in f:
     url = url.strip()
-    logging.debug("This is images url: %s" % (url))
+    logger.info("This is images url: %s" % (url))
     resp = requests.get(url)
     img = resp.raw
     # save raw img on hard disc
     filename = url.split('//')[-1].replace('/', '_')
-    logging.debug("This is images file name: %s" % (filename))
+    logger.info("This is images file name: %s" % (filename))
     with open(filename, "wb") as img_file:
         img_file.write(resp.content)
         img_file.close()
