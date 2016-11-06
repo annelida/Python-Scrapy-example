a737faba34da6a30172789176c4eefcd149945e3
annelida <viqsiq@gmail.com>
2016-11-06 10:00:22 +0200

    Full path to files was added, set time.sleep

--- thumb_collector.py
+++ thumb_collector.py
@@ -1,5 +1,7 @@
+import os
 import requests
 import logging
+import time
 
 logger = logging.getLogger('thumbnails')
 hdlr = logging.FileHandler('thumbnails.log')
@@ -8,15 +10,18 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
-f = open('test_words.csv')
+f = open('img_urls.csv')
 for url in f:
     url = url.strip()
     logger.info("This is images url: %s" % (url))
     resp = requests.get(url)
     img = resp.raw
+    time.sleep(20)
     # save raw img on hard disc
-    filename = url.split('//')[-1].replace('/', '_')
+    path = '/home/vikky/Desktop/DVCS/yann_decoopman/images/'
+    filename = url.split('//')[-1].replace('/', '_') + '.jpg'
     logger.info("This is images file name: %s" % (filename))
+    fullpath = os.path.join(path, filename)
     with open(filename, "wb") as img_file:
         img_file.write(resp.content)
         img_file.close()
